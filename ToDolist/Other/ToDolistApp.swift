717140f23039a7c3fe4fe91306775a2b81bd50d8
Shubham Sharma <shubham-1.sharma@alcon.com>
2024-03-25 20:03:07 +0530

    Firebase library added

---
+++ ToDolist/Other/ToDolistApp.swift
@@ -0,0 +1,23 @@
+//
+//  ToDolistApp.swift
+//  ToDolist
+//
+//  Created by Sharma, Shubham on 25/03/24.
+//
+
+import FirebaseCore
+import SwiftUI
+
+@main
+struct ToDolistApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
